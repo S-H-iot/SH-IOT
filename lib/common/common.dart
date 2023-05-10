4179c85fb77e46d44b42d244ded1c48e3a2a681b
Lovingcats <whehdrms6810@naver.com>
2023-05-10 13:39:25 +0900

    [DELETE] 전 프로젝트

--- lib/common/common.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CommonColor {
-  //색을 정리해둔 코드
-  static Color blue = const Color(0xFFC8D1F4);
-  static Color pink = const Color(0xFFFFD5D5);
-  static Color purple = const Color(0xFFE4D1EB);
-}
--- lib/model/clothes.dart
+++
@@ -1,5 +0,0 @@
-class Clothes {
-  final String url;
-  final String name;
-  Clothes(this.url, this.name);
-}
